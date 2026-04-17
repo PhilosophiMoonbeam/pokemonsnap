fb64cf702bc6cd2283993fb00dc797c4eb141d58
PhilosophiMoonbeam <94211695+PhilosophiMoonbeam@users.noreply.github.com>
2026-04-17 00:49:25 -0400

    refactor: isolate func_80003530 copy count

--- src/sys/dma.c
+++ src/sys/dma.c
@@ -442,8 +442,12 @@ void func_80003530(u16* data, u8* outBuffer) {
                 lengthsCursor = !value ? lengthsCursor->left : lengthsCursor->right;
             }
             VPK0_MEM_GET_BITS(value, lengthsCursor->value);
-            while (value-- > 0) {
-                *(outPtr++) = *(copySrc++);
+            {
+                s32 copyCount = value;
+
+                while (copyCount-- > 0) {
+                    *(outPtr++) = *(copySrc++);
+                }
             }
         }
     }
