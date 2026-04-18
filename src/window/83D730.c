5a3469e878beac406f402f3180375656ded2b24d
PhilosophiMoonbeam <94211695+PhilosophiMoonbeam@users.noreply.github.com>
2026-04-17 21:32:10 -0400

    refactor: recompute UIMem_Reallocate copy size from data

--- src/window/83D730.c
+++ src/window/83D730.c
@@ -195,7 +195,7 @@ u8* UIMem_Reallocate(u8* data, u32 size) {
         return NULL;
     }
 
-    oldSize = chunk->size;
+    oldSize = (GET_CHUNK(data))->size;
 
     for (i = 0; i < oldSize; i++) {
         newData[i] = data[i];
