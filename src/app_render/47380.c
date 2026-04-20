7bab5d3947a6a793a7f66cba33b72b079118d816
PhilosophiMoonbeam <94211695+PhilosophiMoonbeam@users.noreply.github.com>
2026-04-20 00:32:50 -0400

    refactor: drop unused 47380 playback helper arg

--- src/app_render/47380.c
+++ src/app_render/47380.c
@@ -1371,7 +1371,7 @@ static s32 func_8009E3D0_pickMask(s32 dim) {
     return G_TX_NOMASK;
 }
 
-static void func_8009E3D0_applyPlaybackState(EffectPhotoData* effect, EffectSprites* sprites, s32* alphaCompare, s32* blendColorA) {
+static void func_8009E3D0_applyPlaybackState(EffectPhotoData* effect, s32* alphaCompare, s32* blendColorA) {
     u32 playbackFlags;
 
     playbackFlags = FX_UNPACK_PHOTO_RENDER_FLAGS(effect->packedPlaybackFlags);
@@ -1409,7 +1409,6 @@ static void func_8009E3D0_applyPlaybackState(EffectPhotoData* effect, EffectSpri
         }
     }
 
-    (void) sprites;
 }
 
 void func_8009E3D0(GObj* gobj) {
@@ -1642,7 +1641,7 @@ void func_8009E3D0(GObj* gobj) {
             loadedTexture = textureData;
         }
 
-        func_8009E3D0_applyPlaybackState(effect, sprites, &alphaCompare, &blendColorA);
+        func_8009E3D0_applyPlaybackState(effect, &alphaCompare, &blendColorA);
 
         depth = (s32) ((vpTransZ + clipZ * vpScaleZ) * 32.0f);
         gDPSetPrimDepth(gMainGfxPos[0]++, depth, 0);
